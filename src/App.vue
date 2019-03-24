30d89dfe77ed936ba0c32dfa3d5dc014c1ff168f
klx <1148273143@qq.com>
2019-03-24 16:17:12 +0800

    完成主页布局

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header >header</el-header>
+      <el-main>main</el-main>
+      <el-footer height="100px">
+        <el-row>
+          <el-col :span="6">企业合作</el-col>
+          <el-col :span="6">联系我们</el-col>
+          <el-col :span="6">意见反馈</el-col>
+          <el-col :span="6">友情链接</el-col>
+        </el-row>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
@@ -10,14 +20,26 @@ export default {
   name: 'App'
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  .el-header{
+    -webkit-box-shadow: 0px 10px 5px #888888;
+    -moz-box-shadow: 0px 10px 5px #888888;
+    box-shadow: 0px 10px 5px #888888;
+  }
+  .el-main{
+    /*height: 1000px;*/
+  }
+  .el-footer{
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #1c1f21;
+    color: #838a8e;
+    padding: 0px 20% 10px 20%;
+  }
 </style>
