075239dcd63ad0ae650521c9ef44e5eb3c7b5a66
klx <1148273143@qq.com>
2019-03-24 15:38:36 +0800

    引入element-ui

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
+
 //引入axios
 import axios from "axios"
 
