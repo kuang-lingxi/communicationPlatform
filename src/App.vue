f4d264daf639b747235879e6d17ab450200bb1d9
klx <1148273143@qq.com>
2019-03-26 23:04:47 +0800

    完成主页基本布局

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
   <div id="app">
     <el-container>
-      <el-header >header</el-header>
-      <el-main>main</el-main>
+      <el-header>
+
+        <el-row style="line-height: 60px;">
+          <el-col :span="2">
+            <img src="../static/images/buct.jpg" alt="" height="60px" width="60px">
+          </el-col>
+          <el-col :span="2">全部课程</el-col>
+          <el-col :span="2">在线论坛</el-col>
+          <el-col :span="2">资源上传</el-col>
+          <el-col :span="10">
+            <el-input
+              placeholder="请输入内容"
+              v-model="input">
+              <i slot="prefix" class="el-input__icon el-icon-search"></i>
+            </el-input>
+          </el-col>
+          <el-col :offset="1" :span="2" style="line-height: 60px">
+            我的课程
+            <i class="el-icon-bell" style="font-size:25px;margin-left: 10px;"></i>
+          </el-col>
+          <el-col :span="1" style="line-height: 60px">
+            <img :src="userImg" alt="" class="userImg">
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
       <el-footer height="100px">
         <el-row>
           <el-col :span="6">企业合作</el-col>
@@ -17,13 +43,29 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      userImg:'../static/images/userImg.jpg',
+      input:'',
+    };
+  }
 }
 </script>
 <style>
   *{
     margin: 0;
     padding: 0;
+  }
+  .userImg{
+    -webkit-border-radius: 50px;
+    -moz-border-radius: 50px;
+    border-radius: 50px;
+    height: 60px;
+    width:60px;
+  }
+  .el-container{
+    height: 100%;
   }
   .el-header{
     -webkit-box-shadow: 0px 10px 5px #888888;
@@ -42,4 +84,6 @@ export default {
     color: #838a8e;
     padding: 0px 20% 10px 20%;
   }
+
+
 </style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <main class="main">
+      <el-row>
+        <el-col :span="6" class="mainLeft">
+          <div class="item" v-for="val in item">
+            <div class="item">
+              {{val.msg}}
+              <i class="el-icon-arrow-right"></i>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="18">
+          <el-carousel>
+            <el-carousel-item v-for="item in 4" :key="item">
+              {{item}}
+            </el-carousel-item>
+          </el-carousel>
+          <el-row style="padding: 10px">
+                <span class="mainBottom" v-for="item in bottom" :key="item.id">
+                  <img class="mainBottomImg" :src="item.imgUrl" alt="">
+                  <p class="mainBottomTitle">{{item.title}}</p>
+                  <p class="mainBottomContent">{{item.content}}</p>
+                </span>
+          </el-row>
+        </el-col>
+      </el-row>
+    </main>
+    <div style="width: 100%;text-align: center;font-weight: bold;margin: 20px 0;">课程推荐</div>
+    <div class="recommendedMain">
+      <span class="recommendeContent" v-for="item in recommende" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+        <p>{{item.title}}</p>
+        <p>{{item.content}}</p>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "home",
+        data() {
+          return {
+            item:[
+              {id:0, msg:'前沿/区块链/人工智能'},
+              {id:1, msg:'前沿/区块链/人工智能'},
+              {id:2, msg:'前沿/区块链/人工智能'},
+              {id:3, msg:'前沿/区块链/人工智能'},
+              {id:4, msg:'前沿/区块链/人工智能'},
+              {id:5, msg:'前沿/区块链/人工智能'},
+              {id:6, msg:'前沿/区块链/人工智能'},
+            ],
+            bottom:[
+              {id:0, title:'Web前端攻城狮子', content:'互联网时代最火爆的技术', imgUrl:'../static/images/userImg.jpg'},
+              {id:1, title:'Web前端攻城狮子', content:'互联网时代最火爆的技术', imgUrl:'../static/images/userImg.jpg'},
+              {id:2, title:'Web前端攻城狮子', content:'互联网时代最火爆的技术', imgUrl:'../static/images/userImg.jpg'},
+              {id:3, title:'Web前端攻城狮子', content:'互联网时代最火爆的技术', imgUrl:'../static/images/userImg.jpg'},
+              {id:4, title:'Web前端攻城狮子', content:'互联网时代最火爆的技术', imgUrl:'../static/images/userImg.jpg'},
+            ],
+            recommende:[
+              {id:0, imgUrl:'', title:'1', content:'11'},
+              {id:1, imgUrl:'', title:'2', content:'22'},
+              {id:2, imgUrl:'', title:'3', content:'33'},
+              {id:3, imgUrl:'', title:'4', content:'44'},
+              {id:4, imgUrl:'', title:'5', content:'55'}
+            ]
+          }
+        }
+    }
+</script>
+
+<style scoped>
+  /*走马灯底部链接*/
+  .main{
+    margin: 10px auto;
+    border:1px solid black;
+    -webkit-border-radius: 7px;
+    -moz-border-radius: 7px;
+    border-radius: 7px;
+    width:65%;
+  }
+  .mainLeft{
+    background-color: #2b333b;
+    color:#aaadb1;
+  }
+  .mainBottom{
+    width: 19%;
+    /*border:1px solid black;*/
+    display: inline-block;
+    text-align: center;
+    margin-top: 10px;
+  }
+  .mainBottomImg{
+    width:30px;
+    height: 30px;
+    -webkit-border-radius: 50px;
+    -moz-border-radius: 50px;
+    border-radius: 50px;
+  }
+  .mainBottomTitle{
+    font-size: 10px;
+    margin-top:10px;
+    margin-bottom: 5px;
+  }
+  .mainBottomContent{
+    font-size: 10px;
+  }
+  /*走马灯底部链接*/
+
+  .item{
+    padding: 10px;
+  }
+  /*走马灯效果*/
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+  /*走马灯效果*/
+
+  /*推荐课程*/
+  .recommendedMain{
+    margin: 0px auto;
+    width:65%;
+    border: 1px solid black;
+  }
+  .recommendeContent{
+    display: inline-block;
+    width:20%
+  }
+</style>
